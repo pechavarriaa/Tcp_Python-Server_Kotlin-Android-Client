f32138b20560a2f512cf5c2faae5e2a5db8b9d8e
pechavarriaa <pechavarriaa@gmail.com>
2020-03-29 20:33:12 -0600

    corrected path for  db sqlite

--- Server/webserver/config.py
+++ Server/webserver/config.py
@@ -3,8 +3,5 @@ import os
 SECRET_KEY = 'pserver#4$secret#$%!key@'
 SECURITY_PASSWORD_SALT = 'pserverSecurity'
 # SQLALCHEMY CONFIG
-try:
-    SQLALCHEMY_DATABASE_URI = os.environ["DBPATH"]
-except KeyError:
-   print("Please set the environment variable DBPATH")
+SQLALCHEMY_DATABASE_URI = 'sqlite:///pserver.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
