095406e4a48a0343e2cc8b08b9cbdd9c44ff2412
pechavarriaa <pechavarriaa@gmail.com>
2020-03-31 09:06:29 -0600

    added new resources

--- Client/app/src/main/java/com/example/client/MainActivity.kt
+++ Client/app/src/main/java/com/example/client/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.SparseBooleanArray
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,35 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Initializing the array lists and the adapter
-        var itemlist = arrayListOf<String>()
-        var adapter =ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_multiple_choice
-            , itemlist)
-        // Adding the items to the list when the add button is pressed
-        add.setOnClickListener {
 
-            itemlist.add(editText.text.toString())
-            listView.adapter =  adapter
-            adapter.notifyDataSetChanged()
-            // This is because every time when you add the item the input space or the eidt text space will be cleared
-            editText.text.clear()
-        }
-
-        // Selecting and Deleting the items from the list when the delete button is pressed
-        delete.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    adapter.remove(itemlist[item])
-                }
-                item--
-            }
-            position.clear()
-            adapter.notifyDataSetChanged()
-        }
     }
 }
