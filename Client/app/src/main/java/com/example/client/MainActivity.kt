f321e8ae57e116f4cbc25464b35b3745eb8bfa8c
pechavarriaa <pechavarriaa@gmail.com>
2020-03-31 22:47:16 -0600

    try main and added done in call tcpserver

--- Client/app/src/main/java/com/example/client/MainActivity.kt
+++ Client/app/src/main/java/com/example/client/MainActivity.kt
@@ -10,9 +10,6 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 
-
-
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,31 +27,27 @@ class MainActivity : AppCompatActivity() {
             var jsonString = gson.toJson(GetTodos("pechavarriaa", "pepe25", true))
             var tcpClientTodo = TcpClient(jsonString)
             var itemlist = arrayListOf<String>()
-            var adapter =ArrayAdapter<String>(this,
+            var adapter = ArrayAdapter<String>(
+                this,
                 android.R.layout.simple_list_item_multiple_choice
-                , itemlist)
+                , itemlist
+            )
             // Adding the items to the list when the add button is pressed
 
-            if("200" !in tcpClientTodo.serverResponse ){
-                Toast.makeText(applicationContext, "Message Failed", Toast.LENGTH_SHORT).show()}
-            else {
+            if ("200" !in tcpClientTodo.serverResponse) {
+                Toast.makeText(applicationContext, "Message Failed", Toast.LENGTH_SHORT).show()
+            } else {
                 val jsonObj = JSONObject(tcpClientTodo.serverResponse)
                 val unA: JSONArray = jsonObj.getJSONArray("todos")
-
                 for (i in 0 until unA.length()) {
                     val c = unA.getJSONObject(i)
-                    val title = c.getString("title")
-                    val body = c.getString("body")
+                    val idT = c.getInt("id")
+                    val itemT = c.getString("item")
+                    val doneT = c.getBoolean("done")
+                    itemlist.add(itemT)
+
                 }
-                data class todo(
-                val id: Int,
-                val todo:String,
-                val done:Boolean
-            )
-            var testModel = gson.fromJson(jsonString, todo::class.java)
-          for ()
-                itemlist.add(editText.text.toString())
-                listView.adapter =  adapter
+                listView.adapter = adapter
                 adapter.notifyDataSetChanged()
             }
         }
