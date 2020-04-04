2e252ddf2c19037bbca89bac18a0556bdcbfedd6
pechavarriaa <pechavarriaa@gmail.com>
2020-04-04 04:11:30 -0600

    ignore configuration

--- Server/config.py
+++
@@ -1,7 +0,0 @@
-import os
-# SECURITY
-SECRET_KEY = 'pserver#4$secret#$%!key@'
-SECURITY_PASSWORD_SALT = 'pserverSecurity'
-# SQLALCHEMY CONFIG
-SQLALCHEMY_DATABASE_URI = 'sqlite:///pserver.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
